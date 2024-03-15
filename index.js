77761f1d97c8fe61a0d987b919a352b23ee623f1
Julie Zenou <me@juliezen.com>
2024-03-15 14:31:25 +1100

    add different sounds for different drums

--- index.js
+++ index.js
@@ -8,6 +8,38 @@ function addListenerToButton (button) {
 }
 
 function handleClick() {
-    var drumSound = new Audio("./sounds/crash.mp3");
-    drumSound.play();
+    
+    switch (this.innerHTML) {
+        case "w":
+            new Audio("./sounds/crash.mp3").play();
+            break;
+        
+        case "a":
+            new Audio("./sounds/kick-bass.mp3").play();
+            break;
+
+        case "s":
+            new Audio("./sounds/snare.mp3").play();
+            break;
+
+        case "d":
+            new Audio("./sounds/tom-1.mp3").play();
+            break;
+
+        case "j":
+            new Audio("./sounds/tom-2.mp3").play();
+            break;
+
+        case "k":
+            new Audio("./sounds/tom-3.mp3").play();
+            break;
+
+        case "l":
+            new Audio("./sounds/tom-4.mp3").play();
+            break;
+
+        default:
+            new Audio("./sounds/crash.mp3").play();
+            break;
+    }
 }
